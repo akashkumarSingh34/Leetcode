ffaa25c5daf67eb1ef5c585aebeff6238cd9e5e7
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2022-05-15 16:02:26 +0530

    Delete Sort Colors.cpp

--- Sort Colors.cpp
+++
@@ -1,9 +0,0 @@
-void sortColors(vector<int>& nums) {
-        for(int i =0; i<nums.size(); i++)
-            for(int j=0; j<i;j++)
-                if(nums[i]<nums[j]){
-                    int temp =nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;   
-                }
-    }

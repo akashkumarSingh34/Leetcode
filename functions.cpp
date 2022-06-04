a8ad3def107d1c27206cac57bc54d8706424e534
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2022-06-04 19:17:17 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,12 @@
+&  ->  bit wise and
+
+|  ->  bit wise or
+
+^  ->  XOR operator
+
+~  ->  Not operator (usually flips bits)
+
+
 trunc(log10(num)) + 1 // tells the length of the number
 
 s= s.substr(0, i) // copy a substring from a string
